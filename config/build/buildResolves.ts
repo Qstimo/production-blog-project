4a6a5f0d490f22381308c6b6979d6e660f5bad71
Qstimo <vadtolpekin@gmail.com>
2023-12-25 18:36:35 +0300

    storyType

--- config/build/buildResolves.ts
+++ config/build/buildResolves.ts
@@ -1,7 +1,7 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
-export function buildResolves(options:BuildOptions):webpack.ResolveOptions {
+export function buildResolves(options: BuildOptions): webpack.ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
         preferAbsolute: true,
