fa373fd5d380f9706528a6f7e6583fc0919002a0
Qstimo <vadtolpekin@gmail.com>
2023-12-26 14:37:47 +0300

    mod

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,19 +2,18 @@ import './styles/index.scss';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import React, { Suspense } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import { classNames } from '../helpers/classNames/classNames/classNames';
 import { AppRouter } from './providers/router';
 
 const App = () => {
-    // React.useEffect(() => {
-    //     throw new Error()
-    // }, [])
     const { theme } = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <Modal />
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
