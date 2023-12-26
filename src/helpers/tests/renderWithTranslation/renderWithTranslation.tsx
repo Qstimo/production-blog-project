318f16be7108d78ce7f1190dc896a37f25208087
Qstimo <vadtolpekin@gmail.com>
2023-12-26 13:13:37 +0300

    @Qstimo

--- src/helpers/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/helpers/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,12 +1,20 @@
 import { render } from '@testing-library/react';
 import React, { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { MemoryRouter } from 'react-router-dom';
 import i18ForTests from '../../../shared/config/i18n/i18nForTest';
 
-export function renderWithTranslation(component: ReactNode) {
+export interface componentRenderOptions {
+    route?: string;
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+    const { route = '/' } = options;
     return render(
-        <I18nextProvider i18n={i18ForTests}>
-            {component}
-        </I18nextProvider>,
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18ForTests}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>,
     );
 }
